7c604eeca64c50f28abcacc24eb95d01f80f419c
mu1633 <2442921851@qq.com>
2023-04-16 11:29:07 +0800

    add task pool

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -6,13 +6,22 @@ import "./Task.sol";
 import "./Order.sol";
 
 contract Marketplace {
+
+    struct TaskInPool {
+    uint256 taskId;
+    uint256 orderId;
+    Task.TaskType taskType;
+    }   
+
     Payment private paymentContract;
     Task private taskContract;
     address[] private workers;
     address private admin;
-
+    TaskInPool[] private taskPool;
+    mapping(address => uint256) public workerLoad;
     mapping(uint256 => Order) public orders;
     uint256 private nextOrderId;
+
 
     modifier onlyAdmin() {
         require(msg.sender == admin, "Only admin can perform this operation.");
@@ -56,18 +65,78 @@ contract Marketplace {
 
 
     function confirmOrder(uint256 orderId, uint256 paymentAmount) public {
-        Order order = orders[orderId];
-        require(msg.sender == order.client(), "Only the client can perform this operation.");
-        require(!order.isConfirmed(), "Order already confirmed.");
-        require(workers.length > 0, "No workers available.");
+    Order order = orders[orderId];
+    require(msg.sender == order.client(), "Only the client can perform this operation.");
+    require(!order.isConfirmed(), "Order already confirmed.");
+    require(workers.length > 0, "No workers available.");
 
-        paymentContract.transfer(address(this), paymentAmount);
-        order.confirm(paymentAmount);
+    paymentContract.transfer(address(this), paymentAmount);
+    order.confirm(paymentAmount);
 
-        // Randomly assign a worker
-        address randomWorker = selectRandomWorker();
-        taskContract.assignTask(order.taskId(), randomWorker);
+    // Add tasks to the TaskPool
+    taskPool.push(TaskInPool(order.trainTaskId(), orderId, Task.TaskType.Training));
     }
+
+    function assignTaskFromPool() public {
+    require(taskPool.length > 0, "No tasks in the pool.");
+
+    // Get the next task from the pool
+    TaskInPool memory task = taskPool[0];
+
+    // Find a worker with enough available capacity
+    address worker = findAvailableWorker();
+    require(worker != address(0), "No available worker found.");
+
+    // Assign the task to the worker
+    taskContract.assignTask(task.taskId, worker);
+    workerLoad[worker]++;
+
+    // Remove the task from the pool
+    taskPool[0] = taskPool[taskPool.length - 1];
+    taskPool.pop();
+    }
+
+    function findAvailableWorker() private view returns (address) {
+    uint256 minLoad = type(uint256).max;
+    address availableWorker = address(0);
+
+    for (uint256 i = 0; i < workers.length; i++) {
+        if (workerLoad[workers[i]] < minLoad) {
+            minLoad = workerLoad[workers[i]];
+            availableWorker = workers[i];
+        }
+    }
+
+    return availableWorker;
+    }
+
+    function setMarketplaceAddress(address marketplaceAddress) external {
+    require(msg.sender == admin, "Only admin can set the marketplace address.");
+    marketplace = marketplaceAddress;
+    }
+
+    function completeTask(uint256 taskId) public {
+        TaskInfo storage task = tasks[taskId];
+        require(task.status == TaskStatus.Assigned, "Task is not in Assigned status.");
+        require(task.worker == msg.sender, "Only assigned worker can complete the task.");
+        task.status = TaskStatus.Completed;
+        emit TaskCompleted(taskId, msg.sender);
+
+        // Notify the Marketplace contract
+        Marketplace(marketplace).onTaskCompleted(taskId, msg.sender);
+    }
+
+    function onTaskCompleted(uint256 taskId, address worker) external {
+        require(msg.sender == address(taskContract), "Only the Task contract can notify the Marketplace of a completed task.");
+
+        // Update worker's load
+        workerLoad[worker]--;
+
+        // Try to assign a new task from the pool
+        if (taskPool.length > 0) {
+            assignTaskFromPool();
+        }
+   }
 
     function selectRandomWorker() private view returns (address) {
         uint256 randomIndex = uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty))) % workers.length;
--- contracts/Task.sol
+++ contracts/Task.sol
@@ -2,19 +2,28 @@
 pragma solidity ^0.8.0;
 
 contract Task {
-    enum TaskType {TRAIN, VALIDATE}
+    enum TaskType { Training, Validation }
+    enum TaskStatus { Created, Assigned, Completed }
 
-    struct TaskDetails {
+    struct TaskInfo {
+        uint256 id;
         TaskType taskType;
+        TaskStatus status;
         string modelUrl;
         string dataUrl;
         uint256 requiredPower;
-        address assignedWorker;
-        bool completed;
+        address creator;
+        address worker;
     }
 
-    mapping(uint256 => TaskDetails) public tasks;
-    uint256 private nextTaskId;
+    uint256 private nextTaskId = 1;
+    mapping(uint256 => TaskInfo) public tasks;
+
+    event TaskCreated(uint256 indexed taskId, address indexed creator);
+    event TaskAssigned(uint256 indexed taskId, address indexed worker);
+    event TaskCompleted(uint256 indexed taskId);
+    event TaskCompleted(uint256 indexed taskId, address indexed worker);
+
 
     function createTask(
         TaskType taskType,
@@ -23,27 +32,43 @@ contract Task {
         uint256 requiredPower
     ) public returns (uint256) {
         uint256 taskId = nextTaskId++;
-        tasks[taskId] = TaskDetails({
-            taskType: taskType,
-            modelUrl: modelUrl,
-            dataUrl: dataUrl,
-            requiredPower: requiredPower,
-            assignedWorker: address(0),
-            completed: false
-        });
+        tasks[taskId] = TaskInfo(taskId, taskType, TaskStatus.Created, modelUrl, dataUrl, requiredPower, msg.sender, address(0));
+        emit TaskCreated(taskId, msg.sender);
         return taskId;
     }
 
     function assignTask(uint256 taskId, address worker) public {
-        TaskDetails storage task = tasks[taskId];
-        require(task.assignedWorker == address(0), "Task already assigned.");
-        task.assignedWorker = worker;
+        TaskInfo storage task = tasks[taskId];
+        require(task.status == TaskStatus.Created, "Task is not in Created status.");
+        task.status = TaskStatus.Assigned;
+        task.worker = worker;
+        emit TaskAssigned(taskId, worker);
     }
 
     function completeTask(uint256 taskId) public {
-        TaskDetails storage task = tasks[taskId];
-        require(task.assignedWorker != address(0), "Task not assigned.");
-        require(!task.completed, "Task already completed.");
-        task.completed = true;
+        TaskInfo storage task = tasks[taskId];
+        require(task.status == TaskStatus.Assigned, "Task is not in Assigned status.");
+        require(task.worker == msg.sender, "Only assigned worker can complete the task.");
+        task.status = TaskStatus.Completed;
+        emit TaskCompleted(taskId);
+    }
+
+    function assignWorker(uint256 taskId, address worker) external {
+    // Only allow the Marketplace contract to call this function
+    require(msg.sender == marketplaceAddress, "Only the marketplace can assign workers.");
+
+    TaskInfo storage task = tasks[taskId];
+    require(task.status == TaskStatus.Created || task.status == TaskStatus.TrainingCompleted, "Task must be in Created or TrainingCompleted status.");
+    task.status = task.taskType == TaskType.Training ? TaskStatus.TrainingAssigned : TaskStatus.ValidationAssigned;
+    task.worker = worker;
+    emit TaskAssigned(taskId, worker);
+    }
+
+    function completeTask(uint256 taskId) public {
+    TaskInfo storage task = tasks[taskId];
+    require(task.status == TaskStatus.Assigned, "Task is not in Assigned status.");
+    require(task.worker == msg.sender, "Only assigned worker can complete the task.");
+    task.status = TaskStatus.Completed;
+    emit TaskCompleted(taskId, msg.sender); 
     }
 }
