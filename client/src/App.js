885d78dc13bc646d4c3af46debde2d8cad332af1
Bruce Xu <bruce.xu@lxdao.io>
2023-06-20 16:33:53 +1200

    Finish header

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Button, ConfigProvider, Layout, theme } from 'antd';
+
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
 import CreateOrder from './components/CreateOrder';
 import OrderPreview from './components/OrderPreview';
 import Home from './components/Home';
 import UserOrders from './components/UserOrders';
 import { uploadToAzure } from './services/azureUpload';
-import WorkerList from './components/WorkerList'
+import WorkerList from './components/WorkerList';
+
+const { Header, Content, Footer } = Layout;
+
+function MenuLink({ href, children }) {
+  return (
+    <a
+      style={{
+        color: '#fff',
+        textDecoration: 'none',
+        display: 'block',
+        height: 88,
+        lineHeight: '88px',
+        padding: '0 33px',
+      }}
+      href={href}
+    >
+      {children}
+    </a>
+  );
+}
 
 function App() {
-
   const handleUpload = async (files, orderID) => {
     try {
       const path = await uploadToAzure(files, orderID);
@@ -20,18 +43,63 @@ function App() {
     }
   };
 
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create-order" element={<CreateOrder onUpload={handleUpload}/>} />
-          <Route path="/order-preview/:orderId" element={<OrderPreview />} />
-          <Route path="/user-orders" element={<UserOrders />} /> 
-          <Route path="/worker-list" element={<WorkerList />} /> 
-        </Routes>
-      </div>
-    </Router>
+    <ConfigProvider theme={{ token: { colorPrimary: '#6558F5' } }}>
+      <Layout>
+        <Header
+          style={{
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            height: 88,
+            background: '#000',
+          }}
+        >
+          <div>
+            <a href="/">
+              <img style={{ width: 86 }} src="/logo.png" alt="" />
+            </a>
+          </div>
+          <div style={{ display: 'flex' }}>
+            <MenuLink href="/">Demo</MenuLink>
+            <MenuLink href="/">About</MenuLink>
+            <MenuLink href="/">My Order</MenuLink>
+          </div>
+          <div style={{ marginLeft: 'auto', display: 'flex' }}>
+            <Button size="large" type="primary" style={{ marginRight: 20 }}>
+              Train
+            </Button>
+            <ConnectButton></ConnectButton>
+          </div>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Router>
+            <div>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route
+                  path="/create-order"
+                  element={<CreateOrder onUpload={handleUpload} />}
+                />
+                <Route
+                  path="/order-preview/:orderId"
+                  element={<OrderPreview />}
+                />
+                <Route path="/user-orders" element={<UserOrders />} />
+                <Route path="/worker-list" element={<WorkerList />} />
+              </Routes>
+            </div>
+          </Router>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          © 2023 All Rights Reserved by Lab Apex
+        </Footer>
+      </Layout>
+    </ConfigProvider>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,38 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>
 );
 
