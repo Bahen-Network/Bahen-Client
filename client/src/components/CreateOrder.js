07dc50a12b930fa33b379bfd22ba6d1daee162f8
Bruce Xu <bruce.xu@lxdao.io>
2023-06-23 11:03:25 +1200

    Add order details

--- client/src/components/CreateOrder.js
+++ client/src/components/CreateOrder.js
@@ -2,41 +2,20 @@ import React, { useState, useRef } from 'react';
 import { createOrderPreview } from '../services/marketplaceService';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { Typography } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
-import { Button, ConfigProvider, Upload, Select, Divider, Steps } from 'antd';
+import {
+  Button,
+  ConfigProvider,
+  Upload,
+  Select,
+  Divider,
+  Steps,
+  Typography,
+} from 'antd';
+
+import Section from './Section';
 
 const { Title, Paragraph } = Typography;
-
-function Section({ title, children }) {
-  return (
-    <div
-      style={{ borderRadius: 24, backgroundColor: '#1B1B1B', marginBottom: 20 }}
-    >
-      <Title
-        level={3}
-        style={{
-          borderRadius: 24,
-          backgroundColor: '#242424',
-          height: '63px',
-          lineHeight: '63px',
-          padding: '0 24px',
-          marginBottom: 0,
-          color: '#fff',
-        }}
-      >
-        {title}
-      </Title>
-      <div
-        style={{
-          padding: 24,
-        }}
-      >
-        {children}
-      </div>
-    </div>
-  );
-}
 
 const CreateOrder = ({ onUpload }) => {
   const scriptInputRef = useRef();
