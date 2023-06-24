c6c2311048149a89f82cebb8b8e242dbc16f4155
xilinshancun <1643023014@qq.com>
2023-06-24 18:03:58 +0800

    [FIX] delete order connect

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Home.css';
-import { configureMoonbaseAlpha } from '../services/marketplaceService';
-
 const Home = () => {
   return (
     <div className="home-container">
       <h2 style={{ fontSize: 60, fontWeight: 'bold', maxWidth: 950 }}>
         Embrace decentralized computing for AI development.
       </h2>
-      <button className="moonbase-button" onClick={configureMoonbaseAlpha}>
-        Connect to Moonbase Alpha
-      </button>
       <p style={{ fontSize: 20, maxWidth: 660, marginTop: 36 }}>
         Introducing Apex Network, the pioneering decentralized platform
         revolutionizing AI training and dataset transactions.
