c41634d7a1b87b4015144bc8a8600eebc3082526
xilinshancun <1643023014@qq.com>
2023-08-09 23:08:44 +0800

    [ADD] add go

---
+++ src/file-service/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/bnb-chain/greenfield-go-sdk/client"
+	"github.com/bnb-chain/greenfield-go-sdk/types"
+)
+
+const (
+	rpcAddr    = "https://gnfd-testnet-fullnode-tendermint-us.bnbchain.org:443"
+	chainId    = "greenfield_5600-1"
+	privateKey = ""
+)
+
+func main() {
+	account, err := types.NewAccountFromPrivateKey("test", privateKey)
+	if err != nil {
+		log.Fatalf("New account from private key error, %v", err)
+	}
+	cli, err := client.New(chainId, rpcAddr, client.Option{DefaultAccount: account})
+	if err != nil {
+		log.Fatalf("unable to new greenfield client, %v", err)
+	}
+	ctx := context.Background()
+	nodeInfo, versionInfo, err := cli.GetNodeInfo(ctx)
+	if err != nil {
+		log.Fatalf("unable to get node info, %v", err)
+	}
+	log.Printf("nodeInfo moniker: %s, go version: %s", nodeInfo.Moniker, versionInfo.GoVersion)
+	height, err := cli.GetLatestBlockHeight(ctx)
+	if err != nil {
+		log.Fatalf("unable to get latest block height, %v", err)
+	}
+	log.Printf("Current block height: %d", height)
+}
